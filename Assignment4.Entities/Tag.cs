c3292eec5dae2bf201470c382ed2b78e80b458c7
Andreas Wachs Hjalager <ahja@itu.dk>
2021-10-02 22:09:33 +0200

    Implement POCOs and Kanban Context

--- Assignment4.Entities/Tag.cs
+++ Assignment4.Entities/Tag.cs
@@ -1,6 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace Assignment4.Entities
 {
     public class Tag
     {
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Key]
+        public string Name { get; set; }
+
+        public ICollection<Task> tasks { get; set; }
     }
 }
